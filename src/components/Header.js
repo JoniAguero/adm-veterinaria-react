70913ddf6a9d983ee167f1724dad43b3cc8b7eee
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-03 22:16:44 -0300

    base

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+            <h1> Administrador de Turnos Veterinaria </h1>
+      </header>
+    )
+  }
+}
+
+export default Header
